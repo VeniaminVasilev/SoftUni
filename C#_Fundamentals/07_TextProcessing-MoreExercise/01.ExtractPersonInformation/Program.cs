fead7e4b0b4f88284133ff45c77d8fa0fb2a1759
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-05-30 16:34:52 +0300

    text-processing-more-exercises

---
+++ C#_Fundamentals/07_TextProcessing-MoreExercise/01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.ExtractPersonInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+
+                int startIndexOfName = line.IndexOf('@') + 1;
+                int endIndexOfName = line.IndexOf('|');
+                string name = line.Substring(startIndexOfName, endIndexOfName - startIndexOfName);
+
+                int startIndexOfAge = line.IndexOf('#') + 1;
+                int endIndexOfAge = line.IndexOf('*');
+                string age = line.Substring(startIndexOfAge, endIndexOfAge - startIndexOfAge);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
