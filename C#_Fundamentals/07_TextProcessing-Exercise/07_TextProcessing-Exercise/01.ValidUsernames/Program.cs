24c8a5065a01e423f1ee103a26c0a642880f6763
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-05-30 09:22:22 +0300

    30.05.2024

---
+++ C#_Fundamentals/07_TextProcessing-Exercise/07_TextProcessing-Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ").ToArray();
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                bool validUsername = true;
+
+                for (int j = 0; j < usernames[i].Length; j++)
+                {
+                    if (!char.IsLetterOrDigit(usernames[i][j]) && usernames[i][j] != '-' && usernames[i][j] != '_')
+                    {
+                        validUsername = false;
+                    }
+                }
+
+                if (validUsername && usernames[i].Length >= 3 && usernames[i].Length <= 16)
+                {
+                    Console.WriteLine(usernames[i]);
+                }
+            }
+        }
+    }
+}
---
+++ C#_Fundamentals/07_TextProcessing-Exercise/07_TextProcessing-Exercise/03.ExtractFile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example Input: C:\Internal\training-internal\Template.pptx
+
+            string pathToFile = Console.ReadLine();
+            int lastIndexOfDot = pathToFile.LastIndexOf('.');
+            int fileNameStartIndex = pathToFile.LastIndexOf('\\') + 1;
+            int extensionLength = pathToFile.Length - lastIndexOfDot - 1;
+            int fileNameLength = pathToFile.Length - fileNameStartIndex - (pathToFile.Length - lastIndexOfDot);
+            string fileName = pathToFile.Substring(fileNameStartIndex, fileNameLength);
+            string extension = pathToFile.Substring(lastIndexOfDot + 1, extensionLength);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
