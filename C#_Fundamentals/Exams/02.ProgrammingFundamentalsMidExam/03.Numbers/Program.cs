76aaeb0aabdd2d1d57ca58a53b48fa1f517531e2
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-06-12 09:43:10 +0300

    exercise-12-06-2024

---
+++ C#_Fundamentals/Exams/02.ProgrammingFundamentalsMidExam/03.Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _03.Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> numbersAboveAverageValue = new List<int>();
+            double averageValue = numbers.Average();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > averageValue)
+                {
+                    numbersAboveAverageValue.Add(numbers[i]);
+                }
+            }
+
+            numbersAboveAverageValue = numbersAboveAverageValue.OrderByDescending(x => x).ToList();
+
+            if (numbersAboveAverageValue.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else if (numbersAboveAverageValue.Count > 5)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write($"{numbersAboveAverageValue[i]} ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbersAboveAverageValue));
+            }
+        }
+    }
+}
