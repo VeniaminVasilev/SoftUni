91f4775a6e781de4d3ce070329e571b216f67265
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-06-13 14:38:40 +0300

    exercise-13.06.2024

---
+++ C#_Fundamentals/Exams/04.ProgrammingFundamentalsMidExam/02.ShoppingList/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _02.ShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = Console.ReadLine()
+                .Split("!")
+                .ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Go Shopping!")
+                {
+                    Console.WriteLine(string.Join(", ", shoppingList));
+                    break;
+                }
+
+                string[] tokens = command.Split(" ");
+                string action = tokens[0];
+                string item = tokens[1];
+
+                if (action == "Urgent")
+                {
+                    if (!shoppingList.Contains(item))
+                    {
+                        shoppingList.Insert(0, item);
+                    }
+                }
+                else if (action == "Unnecessary")
+                {
+                    if (shoppingList.Contains(item))
+                    {
+                        shoppingList.Remove(item);
+                    }
+                }
+                else if (action == "Correct")
+                {
+                    string newItem = tokens[2];
+
+                    if (shoppingList.Contains(item))
+                    {
+                        int indexOfOldItem = shoppingList.IndexOf(item);
+                        shoppingList[indexOfOldItem] = newItem;
+                    }
+                }
+                else if (action == "Rearrange")
+                {
+                    if (shoppingList.Contains(item))
+                    {
+                        shoppingList.Remove(item);
+                        shoppingList.Add(item);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#_Fundamentals/Exams/04.ProgrammingFundamentalsMidExam/03.HeartDelivery/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _03.HeartDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> neighbourhood = Console.ReadLine()
+                .Split("@")
+                .Select(int.Parse)
+                .ToList();
+
+            int position = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Love!")
+                {
+                    Console.WriteLine($"Cupid's last position was {position}.");
+
+                    bool allNumbersAreLessOrEqualZero = neighbourhood.All(x => x <= 0);
+
+                    if (allNumbersAreLessOrEqualZero)
+                    {
+                        Console.WriteLine("Mission was successful.");
+                    }
+                    else
+                    {
+                        int countOfAllFailedHouses = neighbourhood.Count(x => x > 0);
+                        Console.WriteLine($"Cupid has failed {countOfAllFailedHouses} places.");
+                    }
+                    break;
+                }
+
+                string[] tokens = command.Split(" ");
+                int length = int.Parse(tokens[1]);
+
+                int positionAfterJump = position + length;
+
+                if (positionAfterJump >= neighbourhood.Count)
+                {
+                    positionAfterJump = 0;
+                }
+
+                if (neighbourhood[positionAfterJump] == 0)
+                {
+                    Console.WriteLine($"Place {positionAfterJump} already had Valentine's day.");
+                } else
+                {
+                    neighbourhood[positionAfterJump] -= 2;
+
+                    if (neighbourhood[positionAfterJump] == 0)
+                    {
+                        Console.WriteLine($"Place {positionAfterJump} has Valentine's day.");
+                    }
+                }
+
+                position = positionAfterJump;
+            }
+        }
+    }
+}
