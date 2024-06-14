f732d53e7bcb94b90a31124994b9aed1dc712b01
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-06-14 15:06:31 +0300

    exercise-14.06.2024

---
+++ C#_Fundamentals/Exams/05.ProgrammingFundamentalsMidExam/01.BonusScoringSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.BonusScoringSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = int.Parse(Console.ReadLine());
+            int lectures = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+
+            decimal maxBonus = 0;
+            int attendancesOfStudentWithMaxBonus = 0;
+
+            for (int i = 0; i < students; i++)
+            {
+                int attendances = int.Parse(Console.ReadLine());
+                decimal totalBonus = ((decimal)attendances / lectures) * (5 + additionalBonus);
+
+                if (totalBonus > maxBonus)
+                {
+                    maxBonus = totalBonus;
+                    attendancesOfStudentWithMaxBonus = attendances;
+                }
+            }
+
+            decimal roundedMaxBonus = Math.Ceiling(maxBonus);
+            Console.WriteLine($"Max Bonus: {roundedMaxBonus}.");
+            Console.WriteLine($"The student has attended {attendancesOfStudentWithMaxBonus} lectures.");
+        }
+    }
+}
---
+++ C#_Fundamentals/Exams/05.ProgrammingFundamentalsMidExam/03.Inventory/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _03.Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Craft!")
+                {
+                    Console.WriteLine(string.Join(", ", items));
+                    break;
+                }
+
+                string[] tokens = command.Split(" - ");
+                string action = tokens[0];
+
+                if (action == "Collect")
+                {
+                    string item = tokens[1];
+                    if (!items.Contains(item))
+                    {
+                        items.Add(item);
+                    }
+                }
+                else if (action == "Drop")
+                {
+                    string item = tokens[1];
+                    if (items.Contains(item))
+                    {
+                        items.Remove(item);
+                    }
+                }
+                else if (action == "Combine Items")
+                {
+                    string[] array = tokens[1].Split(":");
+                    string oldItem = array[0];
+                    string newItem = array[1];
+
+                    if (items.Contains(oldItem))
+                    {
+                        int indexOfOldItem = items.IndexOf(oldItem);
+                        items.Insert(indexOfOldItem + 1, newItem);
+                    }
+                }
+                else if (action == "Renew")
+                {
+                    string item = tokens[1];
+                    if (items.Contains(item))
+                    {
+                        items.Remove(item);
+                        items.Add(item);
+                    }
+                }
+            }
+        }
+    }
+}
