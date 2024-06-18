5b99822511c4815cadc6aec1356ddcae36d7e167
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-06-18 10:54:46 +0300

    ExamPreparation-20March2024

---
+++ C#_Fundamentals/Exams/ExamPreparation-20March2024/02.DestinationMapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string locations = Console.ReadLine();
+
+            string pattern = @"([=\/])([A-Z][a-zA-Z]{2,})\1";
+
+            MatchCollection matches = Regex.Matches(locations, pattern);
+
+            List<string> locationMatches = new List<string>();
+            int travelPoints = 0;
+
+            foreach (Match match in matches)
+            {
+                string destination = match.Value;
+                destination = destination.Remove(0, 1);
+                destination = destination.Remove(destination.Length - 1, 1);
+                locationMatches.Add(destination);
+                travelPoints += destination.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", locationMatches)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
---
+++ C#_Fundamentals/Exams/ExamPreparation-20March2024/03.BakeryShop/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _03.BakeryShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> foodTypes = new Dictionary<string, int>();
+            int quantitySold = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Complete") { break; }
+
+                string[] tokens = command.Split(" ");
+                string action = tokens[0];
+                int quantity = int.Parse(tokens[1]);
+                string food = tokens[2];
+
+                if (action == "Receive" && quantity > 0)
+                {
+                    if (foodTypes.ContainsKey(food))
+                    {
+                        foodTypes[food] += quantity;
+                    }
+                    else
+                    {
+                        foodTypes.Add(food, quantity);
+                    }
+                }
+                else if (action == "Sell")
+                {
+                    if (!foodTypes.ContainsKey(food))
+                    {
+                        Console.WriteLine($"You do not have any {food}.");
+                    }
+                    else if (foodTypes.ContainsKey(food))
+                    {
+                        if (foodTypes[food] < quantity)
+                        {
+                            int currentQuantitySold = foodTypes[food];
+                            foodTypes[food] = 0;
+                            Console.WriteLine($"There aren't enough {food}. You sold the last {currentQuantitySold} of them.");
+                            quantitySold += currentQuantitySold;
+                            foodTypes.Remove(food);
+                        }
+                        else
+                        {
+                            foodTypes[food] -= quantity;
+                            Console.WriteLine($"You sold {quantity} {food}.");
+                            quantitySold += quantity;
+
+                            if (foodTypes[food] == 0)
+                            {
+                                foodTypes.Remove(food);
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var foodType in foodTypes)
+            {
+                Console.WriteLine($"{foodType.Key}: {foodType.Value}");
+            }
+            Console.WriteLine($"All sold: {quantitySold} goods");
+        }
+    }
+}
