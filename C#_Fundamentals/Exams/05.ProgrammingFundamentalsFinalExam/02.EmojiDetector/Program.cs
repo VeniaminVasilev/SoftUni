abacc08f3cb4e841708223bc0789bf394a0c2f5e
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-06-20 11:47:48 +0300

    05.ProgrammingFundamentalsFinalExam

---
+++ C#_Fundamentals/Exams/05.ProgrammingFundamentalsFinalExam/02.EmojiDetector/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _02.EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            BigInteger coolThreshold = new BigInteger(1);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    coolThreshold *= int.Parse(input[i].ToString());
+                }
+            }
+
+            string pattern = @"((::|\*\*)[A-Z][a-z]{2,}(\2))";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            int emojisFound = matches.Count;
+            List<string> coolEmojis = new List<string>();
+
+            foreach (Match emoji in matches)
+            {
+                string currentEmoji = emoji.Value;
+                string substring = currentEmoji.Substring(2, currentEmoji.Length - 4);
+                int coolness = 0;
+
+                for (int i = 0; i < substring.Length; i++)
+                {
+                    coolness += (char)substring[i];
+                }
+
+                if (coolness >= coolThreshold)
+                {
+                    coolEmojis.Add(currentEmoji);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+            Console.WriteLine($"{emojisFound} emojis found in the text. The cool ones are:");
+            foreach (string emoji in coolEmojis)
+            {
+                Console.WriteLine(emoji);
+            }
+        }
+    }
+}
