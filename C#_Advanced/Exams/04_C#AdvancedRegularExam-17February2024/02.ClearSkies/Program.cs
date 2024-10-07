55d5c797ad2a9a557270216b43aeaa23ce745480
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-10-07 10:13:32 +0300

    C#AdvancedRegularExam-17February2024

---
+++ C#_Advanced/Exams/04_C#AdvancedRegularExam-17February2024/02.ClearSkies/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace _02.ClearSkies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();
+            }
+
+            int jetRow = 0;
+            int jetCol = 0;
+            int countEnemies = 0;
+            int armor = 300;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row][col] == 'J')
+                    {
+                        jetRow = row;
+                        jetCol = col;
+                        matrix[row][col] = '-';
+                    }
+                    else if (matrix[row][col] == 'E')
+                    {
+                        countEnemies++;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "up")
+                {
+                    jetRow--;
+                }
+                else if (command == "down")
+                {
+                    jetRow++;
+                }
+                else if (command == "left")
+                {
+                    jetCol--;
+                }
+                else if (command == "right")
+                {
+                    jetCol++;
+                }
+
+                if (matrix[jetRow][jetCol] == 'E')
+                {
+                    matrix[jetRow][jetCol] = '-';
+                    countEnemies--;
+
+                    if (countEnemies == 0)
+                    {
+                        Console.WriteLine("Mission accomplished, you neutralized the aerial threat!");
+                        break;
+                    }
+                    else
+                    {
+                        armor -= 100;
+
+                        if (armor <= 0)
+                        {
+                            Console.WriteLine($"Mission failed, your jetfighter was shot down! Last coordinates [{jetRow}, {jetCol}]!");
+                            break;
+                        }
+                    }
+                }
+                else if (matrix[jetRow][jetCol] == 'R')
+                {
+                    armor = 300;
+                    matrix[jetRow][jetCol] = '-';
+                }
+            }
+
+            matrix[jetRow][jetCol] = 'J';
+
+            for (int row = 0; row < n; row++)
+            {
+                Console.WriteLine(string.Join("", matrix[row]));
+            }
+        }
+    }
+}
---
+++ C#_Advanced/Exams/04_C#AdvancedRegularExam-17February2024/03.SharkTaxonomy/Classifier.cs
@@ -0,0 +1,65 @@
+﻿namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+        private int _capacity;
+        private List<Shark> _species;
+
+        public int Capacity { get { return _capacity; } }
+        public List<Shark> Species { get { return _species; } }
+
+        public int GetCount { get { return _species.Count; } }
+
+        public Classifier(int capacity)
+        {
+            _capacity = capacity;
+            _species = new List<Shark>();
+        }
+
+        public void AddShark(Shark shark)
+        {
+            if (_capacity > _species.Count && !_species.Any(s => s.Kind == shark.Kind))
+            {
+                _species.Add(shark);
+            }           
+        }
+
+        public bool RemoveShark(string kind)
+        {
+            if (_species.Any(s => s.Kind == kind))
+            {
+                Shark sharkToBeRemoved = _species.Find(s => s.Kind == kind);
+                _species.Remove(sharkToBeRemoved);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string GetLargestShark()
+        {
+            Shark largestShark = _species.OrderByDescending(s => s.Length).FirstOrDefault();
+            return largestShark.ToString();
+        }
+
+        public double GetAverageLength()
+        {
+            double averageLength = _species.Average(s => s.Length);
+            return averageLength;
+        }
+
+        public string Report()
+        {
+            string report = $"{_species.Count} sharks classified:";
+
+            for (int i = 0; i < _species.Count; i++)
+            {
+                report += $"\n{_species[i].ToString()}";
+            }
+
+            return report;
+        }
+    }
+}
