3cada7217d0e3ad893abdcd91b0dea1b80038ed3
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-11-07 10:57:42 +0200

    Encapsulation-Lab exercises

---
+++ C#_OOP/02_Encapsulation-Lab/4.Team/Person.cs
@@ -0,0 +1,99 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age < 30)
+            {
+                this.salary += ((percentage / 2) / 100) * this.salary;
+            }
+            else
+            {
+                this.salary += (percentage / 100) * this.salary;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} receives {this.salary:f2} leva.";
+        }
+    }
+}
