0eac435675ae196b194afd548ecd0b17a30a8a1c
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-12-11 09:36:44 +0200

    SOLID-Lab

---
+++ C#_OOP/06_SOLID-Lab/P03.Detail_Printer/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    public class Manager : Employee
+    {
+        public Manager(string name, ICollection<string> documents) : base(name)
+        {
+            this.Documents = new List<string>(documents);
+        }
+
+        public IReadOnlyCollection<string> Documents { get; set; }
+
+        public override string Print()
+        {
+            string output = $"{this.Name}\n{string.Join(Environment.NewLine, this.Documents)}";
+
+            return output;
+        }
+    }
+}
