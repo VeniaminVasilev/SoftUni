34da9046d9ab7719759e581aa31278e8ebf92881
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2024-12-23 06:58:47 +0200

    ReflectionAndAttributes-Lab

---
+++ C#_OOP/07_ReflectionAndAttributes-Lab/03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"All Private Methods of Class: {investigatedClass}");
+            stringBuilder.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            foreach (MethodInfo method in classMethods)
+            {
+                stringBuilder.AppendLine(method.Name);
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
