c69232bf2ef1fe2453eeaebf2468062f6624a46f
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2025-01-24 15:00:16 +0200

    DesignPatterns-Lab

---
+++ C#_OOP/10_DesignPatterns-Lab/01.Singleton/SingletonDataContainer.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.Singleton
+{
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> _capitals = new Dictionary<string, int>();
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+
+        public static SingletonDataContainer Instance => instance;
+
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            string[] elements = File.ReadAllLines("capitals.txt");
+            for (int i = 0; i < elements.Length; i += 2)
+            {
+                _capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+
+        public int GetPopulation(string name)
+        {
+            return _capitals[name];
+        }
+    }
+}
