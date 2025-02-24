b58721b23218919266a0657c65d6a6b7b20a8717
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2025-02-24 17:46:43 +0200

    C#_OOP_Regular_Exam_10_August_2024

---
+++ C#_OOP/Exams/2024/C#_OOP_Regular_Exam_10_August_2024/StructureAndBusinessLogic/FootballManager/Models/AmateurManager.cs
@@ -0,0 +1,21 @@
+﻿namespace FootballManager.Models
+{
+    public class AmateurManager : Manager
+    {
+        public AmateurManager(string name) : base(name, 15) { }
+
+        public override void RankingUpdate(double updateValue)
+        {
+            this.Ranking += updateValue * 0.75;
+
+            if (this.Ranking < 0)
+            {
+                this.Ranking = 0;
+            }
+            else if (this.Ranking > 100)
+            {
+                this.Ranking = 100;
+            }
+        }
+    }
+}
---
+++ C#_OOP/Exams/2024/C#_OOP_Regular_Exam_10_August_2024/StructureAndBusinessLogic/FootballManager/Models/Manager.cs
@@ -0,0 +1,43 @@
+﻿using FootballManager.Models.Contracts;
+using FootballManager.Utilities.Messages;
+
+namespace FootballManager.Models
+{
+    public abstract class Manager : IManager
+    {
+        private string _name;
+        private double _ranking;
+
+        public Manager(string name, double ranking)
+        {
+            this.Name = name;
+            this.Ranking = ranking;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.ManagerNameNull);
+                }
+                _name = value;
+            }
+        }
+
+        public double Ranking
+        {
+            get { return _ranking; }
+            protected set { _ranking = value; }
+        }
+
+        public abstract void RankingUpdate(double updateValue);
+
+        public override string ToString()
+        {
+            return $"{this._name} - {this.GetType().Name} (Ranking: {this._ranking:F2})";
+        }
+    }
+}
