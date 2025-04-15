923d550d055c8bdde9de6181cf7f450d82595a07
Veniamin Vasilev <veniaminvasilev@kpmg.com>
2025-04-15 17:32:50 +0300

    2025-04-15-code

---
+++ C#_OOP/Exams/2022/C#_OOP_Regular_Exam_10_December_2022/StructureAndBusinessLogic/Models/Delicacies/Gingerbread.cs
@@ -0,0 +1,11 @@
+﻿namespace ChristmasPastryShop.Models.Delicacies
+{
+    public class Gingerbread : Delicacy
+    {
+        private const double price = 4.00;
+
+        public Gingerbread(string name) : base(name, price)
+        {
+        }
+    }
+}
---
+++ C#_OOP/Exams/2022/C#_OOP_Regular_Exam_10_December_2022/StructureAndBusinessLogic/Models/Delicacies/Stolen.cs
@@ -0,0 +1,11 @@
+﻿namespace ChristmasPastryShop.Models.Delicacies
+{
+    public class Stolen : Delicacy
+    {
+        private const double price = 3.50;
+
+        public Stolen(string name) : base(name, price)
+        {
+        }
+    }
+}
